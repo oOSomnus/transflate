2d6e6b0161546690af73df7941741dff484e1297
Yihang Zhu <yihangz@umich.edu>
2025-01-04 16:33:05 -0800

    <fix> change backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {getToken} from "./utils";
 
-const API = axios.create({ baseURL: 'http://localhost:8080' });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080' });
 
 API.interceptors.request.use((config) => {
     const token = getToken()
